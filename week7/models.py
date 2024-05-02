63525482274f9915e6db103728d30c8d66065e70
NicolasRR <nreateguir@gmail.com>
2024-05-02 20:33:51 +0200

    clean

--- week7/models.py
+++ week7/models.py
@@ -9,18 +9,24 @@ class TwoLayersCNN(nn.Module):
         self.conv1 = nn.Conv2d(
             in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1
         )
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.fc1 = nn.Linear(64 * 56 * 56, 512)
+        self.bn3 = nn.BatchNorm1d(512)
         self.fc2 = nn.Linear(512, num_neurons)
+        self.dropout = nn.Dropout(p=0.5)  # Dropout probability
+
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
         x = x.view(-1, 64 * 56 * 56)
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.bn3(self.fc1(x)))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
 
@@ -32,20 +38,27 @@ class ThreeLayersCNN(nn.Module):
         self.conv1 = nn.Conv2d(
             in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1
         )
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.fc1 = nn.Linear(128 * 56 * 56, 512)
+        self.bn4 = nn.BatchNorm1d(512)
         self.fc2 = nn.Linear(512, num_neurons)
+        self.dropout = nn.Dropout(p=0.5)  # Dropout probability
+
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = F.relu(self.bn3(self.conv3(x)))
         # print(x.shape)
         x = x.view(-1, 128 * 56 * 56)
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
